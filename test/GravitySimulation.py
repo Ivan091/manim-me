9e3ddacb06d60011e56b39bbd062dfd33bb320ab
Ivan Kornasevich <kornasevich@gmail.com>
2021-07-07 19:10:07 +0300

    seven minutes

--- test/GravitySimulation.py
+++ test/GravitySimulation.py
@@ -5,10 +5,11 @@ class Planet(Dot):
     def __init__(self, mass: float, speed: np.ndarray, **kwargs):
         self.mass = mass
         self.speed = speed
+        self.path = []
         super().__init__(radius=mass ** 0.1 * 0.03 + 0.05, **kwargs)
 
     def move(self):
-        self.shift(self.speed)
+        self.path.append(self.path[-1] + self.speed)
 
 
 class GravitySimulation(VGroup):
@@ -16,7 +17,6 @@ class GravitySimulation(VGroup):
             self,
             *planets: Planet,
             gravity_constant: float = 0.00001,
-            frame_time: float = 0.1,
             speed: float = 0.005,
             **kwargs
     ):
@@ -25,57 +25,36 @@ class GravitySimulation(VGroup):
             item.speed *= speed
         self.planets = planets
         self.gravity_constant = gravity_constant
-        self.frame_time = frame_time
 
-    def simulate(self, frames_count: int) -> Succession:
-        animations = []
-        init_centers = [p.copy() for p in self.planets]
-        path = []
-        for f in range(frames_count):
+    def simulate(self, frames_count: int) -> AnimationGroup:
+        [p.path.append(p.get_center()) for p in self.planets]
+        for _ in range(frames_count):
             for p1 in self.planets:
                 for p2 in self.planets:
                     if p1 == p2:
                         continue
-                    p1_center = p1.get_center()
-                    p2_center = p2.get_center()
+                    p1_center = p1.path[-1]
+                    p2_center = p2.path[-1]
 
                     r = np.linalg.norm(p1_center - p2_center)
-
                     p1.speed += ((p2_center - p1_center) / r) * self.gravity_constant * p2.mass / r ** 2
 
-            animations.append(
-                AnimationGroup(
-                    *[p.animate(rate_functions=linear, run_time=self.frame_time).move() for p in self.planets]
-                )
-            )
             for p in self.planets:
                 p.move()
 
-        for j in range(len(init_centers)):
-            self.planets[j].move_to(init_centers[j])
-
-        return Succession(
-            *animations,
-            lag_ratio=self.frame_time
+        return AnimationGroup(
+            *[MoveAlongPath(p, VGroup().set_points_smoothly(p.path), rate_func=linear, run_time=frames_count / 60) for p in self.planets]
         )
 
 
 class Simulation(Scene):
     def construct(self):
         p1 = Planet(1, UP * 10).move_to(LEFT * 2).set_color(RED)
-        tr1 = TracedPath(p1.get_center_of_mass).set_color(RED)
-        p2 = Planet(2, RIGHT * 10).move_to(UP * 2).set_color(GREEN)
-        tr2 = TracedPath(p2.get_center_of_mass).set_color(GREEN)
+        p2 = Planet(20, RIGHT * 10).move_to(UP * 2).set_color(GREEN)
         p3 = Planet(3, DOWN * 10).move_to(RIGHT * 2).set_color(BLUE)
-        tr3 = TracedPath(p3.get_center_of_mass).set_color(BLUE)
         p4 = Planet(4, LEFT * 10).move_to(DOWN * 2).set_color(PURPLE)
-        tr4 = TracedPath(p4.get_center_of_mass).set_color(PURPLE)
-
         sun = Planet(1000, ORIGIN)
 
-        self.wait()
-        self.add(tr1, tr2, tr3, tr4)
         self.play(
-            GravitySimulation(p1, p2, p3, p4, sun).simulate(1000)
+            GravitySimulation(p1, p2, sun).simulate(600)
         )
-        self.wait()
