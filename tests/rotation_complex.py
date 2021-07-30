caad2a145a437a8d32666099ff8106c9323b2a7d
Ivan Kornasevich <kornasevich@gmail.com>
2021-07-30 15:47:06 +0300

    Gitignore update

--- tests/rotation_complex.py
+++ tests/rotation_complex.py
@@ -5,10 +5,6 @@ from _utils.WaitingScene import WaitingScene
 
 class RotationTest(WaitingScene):
     def construct(self):
-        sq = Square(color=ORANGE, side_length=3)
+        sq = Square(color=ORANGE, side_length=2, stroke_width=2)
         self.add(sq)
-        a = np.sin(PI)
-        b = np.cos(PI)
-        point = complex(a, b)
-        point /= abs(point)
-        self.play_wait(sq.copy().set_color(BLUE).animate.apply_complex_function(lambda z: z * point))
+        self.play_wait(sq.copy().set_color(BLUE).animate.apply_complex_function(lambda z: z ** 3))
