4ea982050a8acc379e83c8977ee61ac6fc25c997
Ivan Karnasevich <kornasevich@gmail.com>
2026-02-28 19:48:06 +0100

    add tag_vgroup util function to WaitingScene

--- src/_utils/WaitingScene.py
+++ src/_utils/WaitingScene.py
@@ -1,9 +1,30 @@
+from typing import Any
+
 from manim import *
 from manim.mobject.mobject import _AnimationBuilder
+from manim.typing import Vector3D
 
 
 class WaitingScene(Scene):
     wait_time = 0.2
+
+    def play(
+            self,
+            *args: Animation | Mobject | _AnimationBuilder,
+            **kwargs: Any,
+    ) -> None:
+        super().play(*args, **kwargs)
+        super().play(Wait(self.wait_time))
+
+    def tag_vgroup(self, group: VGroup, color: ManimColor = GREEN, direction: Vector3D = UP):
+        tags = VGroup()
+        for i, vm in enumerate(group):
+            tags.add(
+                Text(str(i), font_size=10).next_to(vm, direction).set_color(color)
+            )
+
+        self.add(tags)
+
 
     def play1(self, *args: Animation | Mobject | _AnimationBuilder, **kwargs):
         self.play(*args, **kwargs)
