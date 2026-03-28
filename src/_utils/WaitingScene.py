e22a2d8c27c8f3fa950f407d4e04fc714b3db481
Ivan Karnasevich <kornasevich@gmail.com>
2026-03-28 11:50:34 +0100

    add conditional scene numbers usage

--- src/_utils/WaitingScene.py
+++ src/_utils/WaitingScene.py
@@ -8,17 +8,19 @@ from manim.typing import Vector3D
 class WaitingScene(Scene):
     wait_time = 0.2
     scene_number = Integer(0).set_color(LIGHT_GRAY).move_to(UP * 3.5 + LEFT * 7)
+    use_scene_number = config.preview
 
     def setup(self) -> None:
         Tex.set_default(font_size=60)
         Text.set_default(font_size=60)
-        self.add(self.scene_number)
+        if self.use_scene_number:
+            self.add(self.scene_number)
 
     def play(
-            self,
-            *args: Animation | Mobject | _AnimationBuilder,
-            wait_time: float | None = wait_time,
-            **kwargs: Any,
+        self,
+        *args: Animation | Mobject | _AnimationBuilder,
+        wait_time: float | None = wait_time,
+        **kwargs: Any,
     ) -> None:
         super().play(*args, **kwargs)
         self.update_scene_number()
@@ -31,7 +33,8 @@ class WaitingScene(Scene):
             self.update_scene_number()
 
     def update_scene_number(self):
-        self.scene_number.set_value(self.scene_number.get_value() + 1)
+        if self.use_scene_number:
+            self.scene_number.set_value(self.scene_number.get_value() + 1)
 
     def tag_vgroup(self, group: VGroup, color: ManimColor = GREEN, direction: Vector3D = UP):
         tags = VGroup()
