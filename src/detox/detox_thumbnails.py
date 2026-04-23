05ae2c4d12ecbb460607c1586ed1aedc64be9250
Ivan Karnasevich <kornasevich@gmail.com>
2026-04-23 12:23:35 +0200

    add Day16

--- src/detox/detox_thumbnails.py
+++ src/detox/detox_thumbnails.py
@@ -57,3 +57,33 @@ class Day8(MeineLiebeScene):
             main_label,
             books_label
         )
+
+class Day11(MeineLiebeScene):
+    def construct(self):
+        books_label = Text("Books done: 0 (for now)").move_to(DOWN * 3)
+        books_label[10].set_color(RED)
+        main_label = Text("Day 11", font_size=120).set_color(BLUE)
+        self.add(
+            main_label,
+            books_label
+        )
+
+class Day13(MeineLiebeScene):
+    def construct(self):
+        books_label = Text("Books done: 0 (for now)").move_to(DOWN * 3)
+        books_label[10].set_color(RED)
+        main_label = Text("Day 13", font_size=120).set_color(BLUE)
+        self.add(
+            main_label,
+            books_label
+        )
+
+class Day16(MeineLiebeScene):
+    def construct(self):
+        books_label = Text("Books done: 0 (for now)").move_to(DOWN * 3)
+        books_label[10].set_color(RED)
+        main_label = Text("Day 16", font_size=120).set_color(BLUE)
+        self.add(
+            main_label,
+            books_label
+        )
